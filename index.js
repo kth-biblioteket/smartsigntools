a2f05e8ab00937c4673fe7b5c2c6aacc3d2502a9
thompa21 <tholind@kth.se>
2023-08-01 09:07:48 +0200

    imas start

--- index.js
+++ index.js
@@ -625,7 +625,7 @@ apiRoutes.get("/imas/smartsignpage", async function (req, res) {
 
 apiRoutes.get("/imas/polopoly", async function (req, res) {
     try {
-        res.render('imaspolopoly', {config: {"externalserverurl" : process.env.EXTERNALSERVERLURL, "lang": req.query.lang || 'sv', "nojquery" : req.query.nojquery}});
+        res.render('imaspolopoly', {config: {"externalserverurl" : process.env.EXTERNALSERVERLURL, "lang": req.query.lang || 'sv', "nojquery" : req.query.nojquery, "start": req.query.start}});
     } catch(err) {
         res.send(err.message)
     }
