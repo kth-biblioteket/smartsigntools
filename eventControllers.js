7e16780c76710adab4b0cd1f94c59b6c7c700ff6
thompa21 <tholind@kth.se>
2023-09-11 22:40:36 +0200

    KTH ny grafisk profil image quality

--- eventControllers.js
+++ eventControllers.js
@@ -1734,7 +1734,7 @@ async function getPageAsImage(events_id, html, template = 'templates/smartsign_t
         await page.setViewport({
             width: width,
             height: height,
-            deviceScaleFactor: 2,
+            deviceScaleFactor: 1,
         });
 
         await page.goto(process.env.SERVERURL + 'smartsigntools/api/v1/calendar/event/' + events_id + '?template=' + template + '&format=' + format + '&orientation=' + orientation, { waitUntil: 'networkidle0' })
