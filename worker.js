bcf3fd8a924b619f641f354467e1635c8959fd7d
Thomas Lind <tholind@kth.se>
2026-03-08 18:09:13 +0100

    pythagoras api

--- worker.js
+++ worker.js
@@ -33,7 +33,7 @@ async function syncAllFloors() {
             try {
                 const url = `${process.env.PYTHAGORAS_API_URL}floor/${floorId}/graphic?incWsComps=false&incFlComps=true&sWMulti=200`;
                 const response = await axios.get(url, {
-                    headers: { 'api_key': process.env.PYTHAGORAS_API_KEY_REF_READ },
+                    headers: { 'api_key': process.env.PYTHAGORAS_API_KEY_READ },
                     timeout: 30000
                 });
 
